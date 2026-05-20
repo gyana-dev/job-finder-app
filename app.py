186db589f7d60eabd49c19c750d85e8390a21c83
gyana-dev <gyanaranjandas88472@gmail.com>
2026-05-20 18:08:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,23 +7,35 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
+
     jobs = []
 
     # Fetch Naukri Jobs
     try:
-        naukri_jobs = fetch_naukri_jobs("AI QA Engineer")
+        naukri_jobs = fetch_naukri_jobs("qa automation engineer")
         jobs.extend(naukri_jobs)
     except Exception as e:
         print("Naukri Error:", e)
 
     # Fetch Indeed Jobs
     try:
-        indeed_jobs = fetch_indeed_jobs("QA Automation Engineer")
+        indeed_jobs = fetch_indeed_jobs("AI QA Engineer")
         jobs.extend(indeed_jobs)
     except Exception as e:
         print("Indeed Error:", e)
 
-    return render_template("index.html", jobs=jobs)
+    # Remove duplicate jobs
+    unique_jobs = []
+    seen = set()
+
+    for job in jobs:
+        key = (job['title'], job['company'])
+
+        if key not in seen:
+            seen.add(key)
+            unique_jobs.append(job)
+
+    return render_template("index.html", jobs=unique_jobs)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
